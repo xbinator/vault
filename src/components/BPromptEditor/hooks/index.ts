f6910a19775e23c282ed20399a789e6b8ade40d8
xbinator <771757562@qq.com>
2026-04-26 23:29:52 +0800

    refactor(BPromptEditor): clean up hooks index

--- src/components/BPromptEditor/hooks/index.ts
+++ src/components/BPromptEditor/hooks/index.ts
@@ -1,6 +1,1 @@
-export { useEditorCore, type EditorCoreOptions } from './useEditorCore';
-export { useEditorSelection, type MenuPosition } from './useEditorSelection';
-export { useEditorTrigger, type TriggerOptions } from './useEditorTrigger';
-export { useEditorKeyboard, type KeyboardOptions } from './useEditorKeyboard';
-export { useEditorPaste, type PasteOptions } from './useEditorPaste';
 export { useVariableEncoder, CARET_SPACER, type VariableEncoderOptions } from './useVariableEncoder';
